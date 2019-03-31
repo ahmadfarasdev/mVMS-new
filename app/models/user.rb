8343abb6abd3f58692754696e13a3a7b3a963817
Irfan <irfanshahzadsandhu@gmail.com>
2019-03-31 19:34:59 +0500

    allow social Sign up for administrative users

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,7 +43,7 @@ class User < ApplicationRecord
   end
   
   def self.get_user(auth)
-    unscoped.where(provider: auth.provider, uid: auth.uid).or(User.unscoped.where(email: auth.info.email || "#{auth.uid}@#{auth.provider}.com")).first
+    unscoped.where(provider: auth.provider, uid: auth.uid,admin: true).or(User.unscoped.where(email: auth.info.email || "#{auth.uid}@#{auth.provider}.com",admin: true)).first
   end
 
   def self.current=(user)
